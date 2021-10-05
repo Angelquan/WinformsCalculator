4c064e7acf512c3828192e5ef05b291082a3fcfa
Angel Quan Castillo <aaq2437@email.vccs.edu>
2021-10-05 15:20:11 -0400

    Add project files.

---
+++ week10project/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace week10project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.LBLGPA = new System.Windows.Forms.Label();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.lblQuizes = new System.Windows.Forms.Label();
+            this.lblAssignments = new System.Windows.Forms.Label();
+            this.txtQ3 = new System.Windows.Forms.TextBox();
+            this.txtT2 = new System.Windows.Forms.TextBox();
+            this.TxtT1 = new System.Windows.Forms.TextBox();
+            this.txtQ1 = new System.Windows.Forms.TextBox();
+            this.txtQ2 = new System.Windows.Forms.TextBox();
+            this.txtAsgnmt1 = new System.Windows.Forms.TextBox();
+            this.txtAsgnmt3 = new System.Windows.Forms.TextBox();
+            this.txtAsgnmt4 = new System.Windows.Forms.TextBox();
+            this.txtAsgnmt2 = new System.Windows.Forms.TextBox();
+            this.txtAsgnmt5 = new System.Windows.Forms.TextBox();
+            this.lblTestAvg = new System.Windows.Forms.Label();
+            this.lblQuizAvg = new System.Windows.Forms.Label();
+            this.lblAsgnmentAvg = new System.Windows.Forms.Label();
+            this.btntest = new System.Windows.Forms.Button();
+            this.btnquiz = new System.Windows.Forms.Button();
+            this.btnassignment = new System.Windows.Forms.Button();
+            this.btntotal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 25);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Grade Average Calculator";
+            // 
+            // LBLGPA
+            // 
+            this.LBLGPA.AutoSize = true;
+            this.LBLGPA.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBLGPA.Location = new System.Drawing.Point(435, 420);
+            this.LBLGPA.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBLGPA.Name = "LBLGPA";
+            this.LBLGPA.Size = new System.Drawing.Size(85, 29);
+            this.LBLGPA.TabIndex = 2;
+            this.LBLGPA.Text = "label1";
+            this.LBLGPA.UseMnemonic = false;
+            this.LBLGPA.Click += new System.EventHandler(this.LBLGPA_Click);
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Location = new System.Drawing.Point(53, 116);
+            this.lblTest.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(40, 17);
+            this.lblTest.TabIndex = 3;
+            this.lblTest.Text = "Test";
+            // 
+            // lblQuizes
+            // 
+            this.lblQuizes.AutoSize = true;
+            this.lblQuizes.Location = new System.Drawing.Point(53, 209);
+            this.lblQuizes.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblQuizes.Name = "lblQuizes";
+            this.lblQuizes.Size = new System.Drawing.Size(58, 17);
+            this.lblQuizes.TabIndex = 4;
+            this.lblQuizes.Text = "Quizes";
+            this.lblQuizes.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblAssignments
+            // 
+            this.lblAssignments.AutoSize = true;
+            this.lblAssignments.Location = new System.Drawing.Point(53, 308);
+            this.lblAssignments.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAssignments.Name = "lblAssignments";
+            this.lblAssignments.Size = new System.Drawing.Size(99, 17);
+            this.lblAssignments.TabIndex = 5;
+            this.lblAssignments.Text = "Assignments";
+            // 
+            // txtQ3
+            // 
+            this.txtQ3.Location = new System.Drawing.Point(534, 209);
+            this.txtQ3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtQ3.Name = "txtQ3";
+            this.txtQ3.Size = new System.Drawing.Size(148, 23);
+            this.txtQ3.TabIndex = 6;
+            // 
+            // txtT2
+            // 
+            this.txtT2.Location = new System.Drawing.Point(377, 116);
+            this.txtT2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtT2.Name = "txtT2";
+            this.txtT2.Size = new System.Drawing.Size(148, 23);
+            this.txtT2.TabIndex = 7;
+            // 
+            // TxtT1
+            // 
+            this.TxtT1.Location = new System.Drawing.Point(217, 116);
+            this.TxtT1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TxtT1.Name = "TxtT1";
+            this.TxtT1.Size = new System.Drawing.Size(148, 23);
+            this.TxtT1.TabIndex = 8;
+            // 
+            // txtQ1
+            // 
+            this.txtQ1.Location = new System.Drawing.Point(217, 209);
+            this.txtQ1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtQ1.Name = "txtQ1";
+            this.txtQ1.Size = new System.Drawing.Size(148, 23);
+            this.txtQ1.TabIndex = 9;
+            // 
+            // txtQ2
+            // 
+            this.txtQ2.Location = new System.Drawing.Point(375, 209);
+            this.txtQ2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtQ2.Name = "txtQ2";
+            this.txtQ2.Size = new System.Drawing.Size(148, 23);
+            this.txtQ2.TabIndex = 10;
+            // 
+            // txtAsgnmt1
+            // 
+            this.txtAsgnmt1.Location = new System.Drawing.Point(216, 308);
+            this.txtAsgnmt1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAsgnmt1.Name = "txtAsgnmt1";
+            this.txtAsgnmt1.Size = new System.Drawing.Size(148, 23);
+            this.txtAsgnmt1.TabIndex = 11;
+            // 
+            // txtAsgnmt3
+            // 
+            this.txtAsgnmt3.Location = new System.Drawing.Point(534, 308);
+            this.txtAsgnmt3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAsgnmt3.Name = "txtAsgnmt3";
+            this.txtAsgnmt3.Size = new System.Drawing.Size(76, 23);
+            this.txtAsgnmt3.TabIndex = 12;
+            // 
+            // txtAsgnmt4
+            // 
+            this.txtAsgnmt4.Location = new System.Drawing.Point(621, 308);
+            this.txtAsgnmt4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAsgnmt4.Name = "txtAsgnmt4";
+            this.txtAsgnmt4.Size = new System.Drawing.Size(79, 23);
+            this.txtAsgnmt4.TabIndex = 13;
+            // 
+            // txtAsgnmt2
+            // 
+            this.txtAsgnmt2.Location = new System.Drawing.Point(375, 308);
+            this.txtAsgnmt2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAsgnmt2.Name = "txtAsgnmt2";
+            this.txtAsgnmt2.Size = new System.Drawing.Size(148, 23);
+            this.txtAsgnmt2.TabIndex = 14;
+            // 
+            // txtAsgnmt5
+            // 
+            this.txtAsgnmt5.Location = new System.Drawing.Point(708, 308);
+            this.txtAsgnmt5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAsgnmt5.Name = "txtAsgnmt5";
+            this.txtAsgnmt5.Size = new System.Drawing.Size(80, 23);
+            this.txtAsgnmt5.TabIndex = 15;
+            // 
+            // lblTestAvg
+            // 
+            this.lblTestAvg.AutoSize = true;
+            this.lblTestAvg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTestAvg.Location = new System.Drawing.Point(931, 119);
+            this.lblTestAvg.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTestAvg.Name = "lblTestAvg";
+            this.lblTestAvg.Size = new System.Drawing.Size(52, 17);
+            this.lblTestAvg.TabIndex = 16;
+            this.lblTestAvg.Text = "label1";
+            this.lblTestAvg.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblQuizAvg
+            // 
+            this.lblQuizAvg.AutoSize = true;
+            this.lblQuizAvg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuizAvg.Location = new System.Drawing.Point(931, 215);
+            this.lblQuizAvg.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblQuizAvg.Name = "lblQuizAvg";
+            this.lblQuizAvg.Size = new System.Drawing.Size(52, 17);
+            this.lblQuizAvg.TabIndex = 17;
+            this.lblQuizAvg.Text = "label3";
+            this.lblQuizAvg.Click += new System.EventHandler(this.label3_Click_1);
+            // 
+            // lblAsgnmentAvg
+            // 
+            this.lblAsgnmentAvg.AutoSize = true;
+            this.lblAsgnmentAvg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAsgnmentAvg.Location = new System.Drawing.Point(931, 314);
+            this.lblAsgnmentAvg.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAsgnmentAvg.Name = "lblAsgnmentAvg";
+            this.lblAsgnmentAvg.Size = new System.Drawing.Size(52, 17);
+            this.lblAsgnmentAvg.TabIndex = 18;
+            this.lblAsgnmentAvg.Text = "label4";
+            this.lblAsgnmentAvg.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btntest
+            // 
+            this.btntest.Location = new System.Drawing.Point(845, 113);
+            this.btntest.Name = "btntest";
+            this.btntest.Size = new System.Drawing.Size(75, 23);
+            this.btntest.TabIndex = 19;
+            this.btntest.Text = "CalcTest";
+            this.btntest.UseVisualStyleBackColor = true;
+            this.btntest.Click += new System.EventHandler(this.btntest_Click);
+            // 
+            // btnquiz
+            // 
+            this.btnquiz.Location = new System.Drawing.Point(801, 203);
+            this.btnquiz.Name = "btnquiz";
+            this.btnquiz.Size = new System.Drawing.Size(119, 23);
+            this.btnquiz.TabIndex = 20;
+            this.btnquiz.Text = "CalcQuiz";
+            this.btnquiz.UseVisualStyleBackColor = true;
+            this.btnquiz.Click += new System.EventHandler(this.btnquiz_Click);
+            // 
+            // btnassignment
+            // 
+            this.btnassignment.Location = new System.Drawing.Point(801, 308);
+            this.btnassignment.Name = "btnassignment";
+            this.btnassignment.Size = new System.Drawing.Size(119, 23);
+            this.btnassignment.TabIndex = 21;
+            this.btnassignment.Text = "CalcAssignment";
+            this.btnassignment.UseVisualStyleBackColor = true;
+            this.btnassignment.Click += new System.EventHandler(this.btnassignment_Click);
+            // 
+            // btntotal
+            // 
+            this.btntotal.Location = new System.Drawing.Point(456, 378);
+            this.btntotal.Name = "btntotal";
+            this.btntotal.Size = new System.Drawing.Size(75, 23);
+            this.btntotal.TabIndex = 22;
+            this.btntotal.Text = "CalcTotal";
+            this.btntotal.UseVisualStyleBackColor = true;
+            this.btntotal.Click += new System.EventHandler(this.btntotal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 554);
+            this.Controls.Add(this.btntotal);
+            this.Controls.Add(this.btnassignment);
+            this.Controls.Add(this.btnquiz);
+            this.Controls.Add(this.btntest);
+            this.Controls.Add(this.lblAsgnmentAvg);
+            this.Controls.Add(this.lblQuizAvg);
+            this.Controls.Add(this.lblTestAvg);
+            this.Controls.Add(this.txtAsgnmt5);
+            this.Controls.Add(this.txtAsgnmt2);
+            this.Controls.Add(this.txtAsgnmt4);
+            this.Controls.Add(this.txtAsgnmt3);
+            this.Controls.Add(this.txtAsgnmt1);
+            this.Controls.Add(this.txtQ2);
+            this.Controls.Add(this.txtQ1);
+            this.Controls.Add(this.TxtT1);
+            this.Controls.Add(this.txtT2);
+            this.Controls.Add(this.txtQ3);
+            this.Controls.Add(this.lblAssignments);
+            this.Controls.Add(this.lblQuizes);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.LBLGPA);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LBLGPA;
+        private System.Windows.Forms.Label lblTest;
+        private System.Windows.Forms.Label lblQuizes;
+        private System.Windows.Forms.Label lblAssignments;
+        private System.Windows.Forms.TextBox txtQ3;
+        private System.Windows.Forms.TextBox txtT2;
+        private System.Windows.Forms.TextBox TxtT1;
+        private System.Windows.Forms.TextBox txtQ1;
+        private System.Windows.Forms.TextBox txtQ2;
+        private System.Windows.Forms.TextBox txtAsgnmt1;
+        private System.Windows.Forms.TextBox txtAsgnmt3;
+        private System.Windows.Forms.TextBox txtAsgnmt4;
+        private System.Windows.Forms.TextBox txtAsgnmt2;
+        private System.Windows.Forms.TextBox txtAsgnmt5;
+        private System.Windows.Forms.Label lblTestAvg;
+        private System.Windows.Forms.Label lblQuizAvg;
+        private System.Windows.Forms.Label lblAsgnmentAvg;
+        private System.Windows.Forms.Button btntest;
+        private System.Windows.Forms.Button btnquiz;
+        private System.Windows.Forms.Button btnassignment;
+        private System.Windows.Forms.Button btntotal;
+    }
+}
+
---
+++ week10project/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week10project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LBLGPA_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btntest_Click(object sender, EventArgs e)
+        {
+            double T1 = double.Parse(TxtT1.Text);
+            double T2 = double.Parse(txtT2.Text);
+            double totalTest = ((T1 + T2) / 2) * .40;
+            lblTestAvg.Text = totalTest.ToString();
+
+
+        }
+
+        private void btnquiz_Click(object sender, EventArgs e)
+        {
+            double q1 = double.Parse(txtQ1.Text);
+            double q2 = double.Parse(txtQ2.Text);
+            double q3 = double.Parse(txtQ3.Text);
+
+            double totalquiz = ((q1 + q2 + q3) / 3) * .30;
+            lblQuizAvg.Text = totalquiz.ToString();
+
+        }
+
+        private void btnassignment_Click(object sender, EventArgs e)
+        {
+            double A1 = double.Parse(txtAsgnmt1.Text);
+            double A2 = double.Parse(txtAsgnmt2.Text);
+            double A3 = double.Parse(txtAsgnmt3.Text);
+            double A4 = double.Parse(txtAsgnmt4.Text);
+            double A5 = double.Parse(txtAsgnmt5.Text);
+
+            double totalAssignment = ((A1 + A2 + A3 + A4 + A5) / 5) * .30;
+            lblAsgnmentAvg.Text = totalAssignment.ToString();
+        }
+
+        private void btntotal_Click(object sender, EventArgs e)
+        {
+            double ta = double.Parse(lblTestAvg.Text);
+            double qa = double.Parse(lblQuizAvg.Text);
+            double aa = double.Parse(lblAsgnmentAvg.Text);
+
+            double totalAverage = (ta + qa + aa);
+            LBLGPA.Text = totalAverage.ToString();
+
+
+        }
+    }
+    
+}
